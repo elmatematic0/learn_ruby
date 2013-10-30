e320bba2a654367f7c48a41f961242d7311f0aae
elmatematic0 <wkdalebout+github@gmail.com>
2013-10-30 17:13:41 -0400

    refactoring timer to cleaner solution.

--- 08_book_titles/timer.rb
+++ 08_book_titles/timer.rb
@@ -1,6 +1,9 @@
 class Timer
+ 
+  HOUR = 3600
+  MINUTE = 60
+  SECOND = 1
 
-  # attr_accessor
   def seconds
     0
   end
@@ -17,36 +20,71 @@ class Timer
   end
 
   def padded(input)
-    if input == 0
-      "00:00:00"
-    elsif input.to_i < 10
-      "00:00:0#{input}"
-    elsif input.to_i >= 10 && input.to_i < 60
-      "00:00:#{input}"
-    elsif input.to_i >= 60 && input.to_i < 3600
-      seconds = input % 60
-      minutes = input / 60
-      if seconds < 10 && minutes < 10
-        "00:0#{minutes}:0#{seconds}"
-      elsif seconds < 10 && minutes >= 10
-        "00:#{minutes}:0#{seconds}"
-      elsif seconds >= 10 && minute >= 10
-        "00:#{minutes}:#{seconds}"
-      end
-    elsif input.to_i >= 3600
-      hours = input / 3600
-      minutes = (input - 3600) / 60
-      seconds = (input - 3600) % 60
-      if seconds < 10 && minutes < 10
-        "0#{hours}:0#{minutes}:0#{seconds}"
-      elsif seconds < 10 && minutes >= 10
-        "0#{hours}:#{minutes}:0#{seconds}"
-      elsif seconds >= 10 && minutes >= 10
-        "0#{hours}:#{minutes}:#{seconds}"
-      elsif seconds >= 10 && minutes < 10
-        "0#{hours}:0#{minutes}:#{seconds}"
+    
+    hour_10 = input/HOUR
+    minute_10 = (input % HOUR)/MINUTE
+    second_10 = (input % MINUTE)/SECOND
+
+      if hour_10 == 0 
+        hours = "00"
+      elsif hour_10 < 10
+        hours = "0#{hour_10}"
+      else
+        hours = "#{hour_10}"
       end
 
-    end
-  end
+      if minute_10 == 0
+        minutes = "00"
+      elsif minute_10 < 10
+        minutes = "0#{minute_10}"
+      else
+        minutes = "#{minute_10}"
+      end
+
+      if second_10 == 0
+        seconds = "00"
+      elsif second_10 < 10
+        seconds = "0#{second_10}"
+      else
+        seconds = "#{second_10}"
+      end
+
+      "#{hours}:#{minutes}:#{seconds}"
+  
 end
+end
+
+
+# def padded(input)
+  #   if input == 0
+  #     "00:00:00"
+  #   elsif input.to_i < 10
+  #     "00:00:0#{input}"
+  #   elsif input.to_i >= 10 && input.to_i < 60
+  #     "00:00:#{input}"
+  #   elsif input.to_i >= 60 && input.to_i < 3600
+  #     seconds = input % 60
+  #     minutes = input / 60
+  #     if seconds < 10 && minutes < 10
+  #       "00:0#{minutes}:0#{seconds}"
+  #     elsif seconds < 10 && minutes >= 10
+  #       "00:#{minutes}:0#{seconds}"
+  #     elsif seconds >= 10 && minute >= 10
+  #       "00:#{minutes}:#{seconds}"
+  #     end
+  #   elsif input.to_i >= 3600
+  #     hours = input / 3600
+  #     minutes = (input - 3600) / 60
+  #     seconds = (input - 3600) % 60
+  #     if seconds < 10 && minutes < 10
+  #       "0#{hours}:0#{minutes}:0#{seconds}"
+  #     elsif seconds < 10 && minutes >= 10
+  #       "0#{hours}:#{minutes}:0#{seconds}"
+  #     elsif seconds >= 10 && minutes >= 10
+  #       "0#{hours}:#{minutes}:#{seconds}"
+  #     elsif seconds >= 10 && minutes < 10
+  #       "0#{hours}:0#{minutes}:#{seconds}"
+  #     end
+
+  #   end
+  # end
